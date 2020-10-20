c7499b8d7dc25efd9e4674f81ed35493013cce8a
Barry Brands <barrybrands02@hotmail.com>
2020-10-20 20:21:10 +0200

    unnecessary code removed

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -22,19 +22,6 @@ class UserController
     public function loginAction() {
         $variables['title'] = "Login";
 
-        $variables['images'][0]['src'] = '/images/car_1.jpg';
-        $variables['images'][0]['alt'] = 'Car 1';
-        $variables['images'][0]['height'] = '600px';
-        $variables['images'][0]['active'] = 'true';
-
-        $variables['images'][1]['src'] = '/images/car_2.jpg';
-        $variables['images'][1]['alt'] = 'Car 2';
-        $variables['images'][1]['height'] = '600px';
-
-        $variables['images'][2]['src'] = '/images/car_3.jpg';
-        $variables['images'][2]['alt'] = 'Car 3';
-        $variables['images'][2]['height'] = '600px';
-
         return $variables;
     }
 }
