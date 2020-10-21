c4f1835fe4de165f16d33df6f5dda798516a163a
Barry Brands <barrybrands02@hotmail.com>
2020-10-21 23:16:34 +0200

    validate js improved

--- public/js/validateinputelements.js
+++ public/js/validateinputelements.js
@@ -1,40 +1,56 @@
 $pwdI = document.getElementById('passwordInput');
+$pFI = document.getElementById('passwordFeedbackI');
 $cfmPwdI = document.getElementById('confirmPasswordInput');
+$cFFI = document.getElementById('confirmPasswordFeedbackI');
 $pFP = document.getElementById('passwordFeedbackP');
+
 $emFP = document.getElementById('emailFeedbackP');
 $emFI = document.getElementById('emailFeedbackI');
 $emI = document.getElementById('emailInput');
 
-var $pwd;
-var $cfmPwd;
-var $em;
+$fNII = document.getElementById('firstNameInputI');
+$fNI = document.getElementById('firstNameInput');
 
 function validatePasswords() {
     setTimeout(function () {
-        $pwd = $pwdI.value;
-        $cfmPwd = $cfmPwdI.value;
 
-        if ($pwd && $cfmPwd) {
+        if ($pwdI.value && $cfmPwdI.value) {
             $pFP.style.display = 'block';
-            if ($pwd === $cfmPwd) {
-                $pFP.innerText = 'The passwords are identical';
-                $pFP.style.color = 'limegreen';
+            if ($pwdI.value === $cfmPwdI.value) {
+                $pFP.style.display = 'none';
+                $cFFI.style.display = 'inline-block ';
+                $cFFI.style.color = 'limegreen';
+                $cFFI.classList.remove('fa-warning');
+                $cFFI.classList.add('fa-check');
+                $pFI.style.display = 'inline-block';
+                $pFI.style.color = 'limegreen';
+                $pFI.classList.remove('fa-warning');
+                $pFI.classList.add('fa-check');
+
             } else {
                 $pFP.innerText = 'The passwords are not identical';
                 $pFP.style.color = 'red';
+                $cFFI.style.display = 'inline-block';
+                $cFFI.style.color = 'red';
+                $cFFI.classList.remove('fa-check');
+                $cFFI.classList.add('fa-warning');
+                $pFI.style.display = 'inline-block';
+                $pFI.style.color = 'red';
+                $pFI.classList.remove('fa-check');
+                $pFI.classList.add('fa-warning');
             }
         } else {
             $pFP.style.display = 'none';
+            $pFI.style.display = 'none';
+            $cFFI.style.display = 'none';
         }
     }, 10);
 }
 
 function validateEmail() {
     setTimeout(function () {
-        $em = $emI.value;
-
-        if ($em) {
-            if (/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test($em)) {
+        if ($emI.value) {
+            if (/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test($emI.value)) {
                 $emFP.style.display = 'none';
                 $emFI.style.display = 'inline-block';
                 $emFI.classList.remove('fa-warning');
@@ -53,4 +69,21 @@ function validateEmail() {
             $emFI.style.display = 'none';
         }
     }, 10);
+}
+
+function validateText(input) {
+    let i = document.getElementById(input.id.replace('Input', 'FeedbackI'));
+
+    setTimeout(function () {
+        if (input.value) {
+            i.style.display = 'inline-block';
+            i.classList.remove('fa-warning');
+            i.classList.add('fa-check');
+            i.style.color = 'limegreen';
+
+        } else {
+            i.style.display = 'none';
+        }
+    }, 10);
+
 }
