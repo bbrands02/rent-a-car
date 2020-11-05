3a6ffcb9740dcf49216df21fbc5e2a00c8ef8687
Barry Brands <barrybrands02@hotmail.com>
2020-11-05 23:42:35 +0100

    upload img added to objectservice

--- src/Service/ObjectService.php
+++ src/Service/ObjectService.php
@@ -5,6 +5,7 @@ namespace App\Service;
 
 use App\Entity\Car;
 use App\Entity\User;
+use Doctrine\Common\Collections\Criteria;
 use Doctrine\ORM\EntityManager;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -12,6 +13,7 @@ use Symfony\Component\HttpFoundation\Session\Session;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+use Symfony\Component\Uid\Uuid;
 
 class ObjectService extends AbstractController
 {
@@ -37,28 +39,48 @@ class ObjectService extends AbstractController
     // Fetches the right repository for this type
     public function fetchRepo($type)
     {
-        $type = "App\\Entity\\".ucwords($type);
+        $type = "App\\Entity\\" . ucwords($type);
         $repo = $this->getDoctrine()->getRepository($type);
 
         return $repo;
     }
 
-    // Fetches all objects of a given class
-    public function getAll($type)
+    // Fetches all objects of a given entity
+    public function getAll($type, $limit = null)
     {
         $repo = $this->fetchRepo($type);
 
         // Actual fetching of objects
-        $objects = $repo->findAll();
+        if ($limit != null && $limit > 0) {
+            $objects = $repo->findBy(array(), array(), 5);
 
+            $criteria = new Criteria();
+            $criteria->where(Criteria::expr()->neq('image', null));
+
+            $objects = $repo->matching($criteria);
+        } else {
+            $objects = $repo->findAll();
+        }
         return $objects;
+    }
+
+
+    // Fetches one object of a given entity
+    public function getOne($type, $id)
+    {
+        $repo = $this->fetchRepo($type);
+
+        // Actual fetching of objects
+        $object = $repo->find($id);
+
+        return $object;
     }
 
     // Uploads object (needs type from html form)
     public function uploadObject($object)
     {
         $type = $object['type'];
-        $object['type'] = "App\\Entity\\".ucwords($object['type']);
+        $object['type'] = "App\\Entity\\" . ucwords($object['type']);
 
         $newObject = $this->createProperties($object);
 
@@ -71,8 +93,8 @@ class ObjectService extends AbstractController
         return;
     }
 
-
-    public function createProperties($obj) {
+    public function createProperties($obj)
+    {
         $newObject = new $obj['type']();
 
         // For each possible property this row of if statements needs to be expanded
@@ -84,6 +106,12 @@ class ObjectService extends AbstractController
         }
         if (!empty($obj['color'])) {
             $newObject->setColor($obj['color']);
+        }
+        if (!empty($obj['image'])) {
+            $uuid = Uuid::v4();
+            move_uploaded_file($obj['image'], 'images/' . $uuid . '.jpg');
+            $obj['image'] = 'images/' . $uuid . '.jpg';
+            $newObject->setImage($obj['image']);
         }
         if (!empty($obj['email'])) {
             $newObject->setEmail($obj['email']);
